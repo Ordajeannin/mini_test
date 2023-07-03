bab81355cecaab58a476238354039a9da0c84f9e
Antoine JEANNIN <ajeannin@paul-f4Br2s2.clusters.42paris.fr>
2023-07-03 16:13:44 +0200

    update

--- main.c
+++ main.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <readline/readline.h>
+
+int main()
+{
+	char	*input;
+
+	while (1)
+	{
+		input = readline("shell>");
+		//process user input
+		free(input);
+	}
+	return 0;
+}
